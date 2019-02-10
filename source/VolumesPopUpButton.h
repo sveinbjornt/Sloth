de47e504649b9d533d31567bf9134ca79881ee40
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2019-02-10 12:55:10 +0000

    Fixed build version incrementation script, 2018->2019

--- source/VolumesPopUpButton.h
+++ source/VolumesPopUpButton.h
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2018, Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
+ Copyright (c) 2018-2019, Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification,
